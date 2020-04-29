2d70e6a8564135369fbdee1d206404f5078e0f4d
Ubuntu <lferreira@ccg.xrcw5gjt3q2u5m21txhnocuolc.ax.internal.cloudapp.net>
2020-04-29 17:29:11 +0000

    update test files

--- autoautoml/autokeras.py
+++ autoautoml/autokeras.py
@@ -8,8 +8,10 @@ class AUTOKERAS():
     def run_example(self):
 
         # Initialize the classifier.
-        clf = ak.StructuredDataClassifier(max_trials=30)
+        clf = ak.StructuredDataRegressor(max_trials=30, loss="mean_absolute_error")
         # x is the path to the csv file. y is the column name of the column to predict.
-        clf.fit(x='PATH_TO/train.csv', y='survived')
+
+        clf.fit(x='./data/churn2.csv', y='churn_probability')
+        
         # Evaluate the accuracy of the found model.
-        #print('Accuracy: {accuracy}'.format(accuracy=clf.evaluate(x='PATH_TO/eval.csv', y='survived')))
+        print('Accuracy: {accuracy}'.format(accuracy=clf.evaluate(x='PATH_TO/eval.csv', y='survived')))
--- autoautoml/autosklearn.py
+++ autoautoml/autosklearn.py
@@ -1,4 +1,4 @@
-import autosklearn.classification
+import autosklearn.regression
 import sklearn.model_selection
 import sklearn.datasets
 import sklearn.metrics
@@ -10,9 +10,18 @@ class AUTOSKLEARN():
 
     def run_example(self):
 
-        X, y = sklearn.datasets.load_digits(return_X_y=True)
-        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=1)
-        automl = autosklearn.classification.AutoSklearnClassifier()
-        automl.fit(X_train, y_train)
-        y_hat = automl.predict(X_test)
-        print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
+        X, y = sklearn.datasets.load_boston(return_X_y=True)
+        feature_types = (['numerical'] * 3) + ['categorical'] + (['numerical'] * 9)
+        X_train, X_test, y_train, y_test = \
+            sklearn.model_selection.train_test_split(X, y, random_state=1)
+
+        automl = autosklearn.regression.AutoSklearnRegressor(
+            time_left_for_this_task=120,
+            per_run_time_limit=30
+        )
+        automl.fit(X_train, y_train, dataset_name='boston',
+                feat_type=feature_types)
+
+        print(automl.show_models())
+        predictions = automl.predict(X_test)
+        print("R2 score:", sklearn.metrics.r2_score(y_test, predictions))
